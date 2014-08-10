f9fd55bc4be7abeee289ea7131f6c74406a9d84a
Manuel Woelker <github@manuel.woelker.org>
2014-08-10 13:04:15 +0200

    core (HttpProxy): add setCacheDirectory() method

--- core/src/main/java/io/github/floto/core/proxy/HttpProxy.java
+++ core/src/main/java/io/github/floto/core/proxy/HttpProxy.java
@@ -1,8 +1,11 @@
 package io.github.floto.core.proxy;
 
 import com.google.common.base.Throwables;
+import org.apache.http.client.cache.Resource;
+import org.apache.http.impl.client.cache.CacheConfig;
+import org.apache.http.impl.client.cache.CachingHttpClientBuilder;
+import org.apache.http.impl.client.cache.FileResourceFactory;
 import org.eclipse.jetty.proxy.ConnectHandler;
-import org.eclipse.jetty.proxy.ProxyServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -10,15 +13,25 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.Servlet;
 import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
 
 public class HttpProxy implements Closeable {
     private Logger log = LoggerFactory.getLogger(HttpProxy.class);
     private int port;
     private Server server;
+    private boolean cachingEnabled = false;
+    private File cacheDirectory;
 
     public HttpProxy(int port) {
         this.port = port;
+    }
+
+    public void setCacheDirectory(File cacheDirectory) {
+
+        this.cacheDirectory = cacheDirectory;
     }
 
     public void start() {
@@ -28,7 +41,7 @@ public class HttpProxy implements Closeable {
 
         // Setup proxy servlet
         ServletContextHandler context = new ServletContextHandler(handlers, "/", ServletContextHandler.SESSIONS);
-        ProxyServlet proxyServlet = new ProxyServlet();
+        Servlet proxyServlet = createProxyServlet();
         ServletHolder proxyServletHolder = new ServletHolder(proxyServlet);
         context.addServlet(proxyServletHolder, "/*");
 
@@ -41,6 +54,35 @@ public class HttpProxy implements Closeable {
             Throwables.propagate(e);
         }
 
+    }
+
+    private Servlet createProxyServlet() {
+        if(cachingEnabled) {
+            return createCachingProxyServlet();
+        }
+        return new org.eclipse.jetty.proxy.ProxyServlet();
+    }
+
+    private ProxyServlet createCachingProxyServlet() {
+        CacheConfig.Builder configBuilder = CacheConfig.custom();
+        configBuilder.setMaxCacheEntries(10000);
+        configBuilder.setMaxObjectSize(2*1024*1024*1024);
+
+        CachingHttpClientBuilder clientBuilder = CachingHttpClientBuilder.create();
+        clientBuilder.setCacheConfig(configBuilder.build());
+
+        PersistentHttpCacheStorage storage = new PersistentHttpCacheStorage(new File(cacheDirectory, "storage"));
+        clientBuilder.setHttpCacheStorage(storage);
+
+        clientBuilder.setResourceFactory(new FileResourceFactory(new File(cacheDirectory, "resources")) {
+            // Do not actually copy files
+            @Override
+            public Resource copy(String requestId, Resource resource) throws IOException {
+                return resource;
+            }
+        });
+
+        return new ProxyServlet(clientBuilder);
     }
 
     public void close() {
