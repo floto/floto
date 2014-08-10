de4dfa9507f13aca2dfcb370695e254d36c22aff
Manuel Woelker <github@manuel.woelker.org>
2014-08-10 21:24:41 +0200

    ui (index): fix capitalization, title

--- server/src/main/resources/io/github/floto/server/assets/js/manifest/FlotoService.js
+++ server/src/main/resources/io/github/floto/server/assets/js/manifest/FlotoService.js
@@ -1,153 +1,156 @@
 (function () {
-	"use strict";
-	app.factory('FlotoService', function ($resource, $http, $rootScope) {
-		var FlotoService = {};
+    "use strict";
+    app.factory('FlotoService', function ($resource, $http, $rootScope) {
+        var FlotoService = {};
 
-		FlotoService.getManifest = function getManifest() {
+        FlotoService.getManifest = function getManifest() {
             return this.manifest;
-		};
+        };
 
         FlotoService.refreshManifest = function refreshManifest() {
             this.manifest = $resource(app.urlPrefix + 'manifest').get();
             this.manifest.$promise.then(function (manifest) {
-                if(manifest.site) {
+                if (manifest.site) {
                     $rootScope.domainName = manifest.site.domainName;
+                    $rootScope.titleSuffix = " - " + manifest.site.domainName;
+
                 } else {
                     $rootScope.domainName = null;
+                    $rootScope.titleSuffix = null;
                 }
             });
         };
 
-		FlotoService.compileManifest = function compileManifest() {
+        FlotoService.compileManifest = function compileManifest() {
             var recompilePromise = $http.post(app.urlPrefix + 'manifest/compile');
-            recompilePromise.finally(function() {
+            recompilePromise.finally(function () {
                 FlotoService.refreshManifest();
             });
             return  recompilePromise;
-		};
+        };
 
-		FlotoService.getContainerStates = function getContainerStates() {
-			return $resource(app.urlPrefix + 'containers/_state').get();
-		};
+        FlotoService.getContainerStates = function getContainerStates() {
+            return $resource(app.urlPrefix + 'containers/_state').get();
+        };
 
-		FlotoService.redeployContainers = function redeployContainers(request) {
-			return $http.post(app.urlPrefix + 'containers/_redeploy', request);
-		};
+        FlotoService.redeployContainers = function redeployContainers(request) {
+            return $http.post(app.urlPrefix + 'containers/_redeploy', request);
+        };
 
-		FlotoService.startContainers = function startContainers(request) {
-			return $http.post(app.urlPrefix + 'containers/_start', request);
-		};
+        FlotoService.startContainers = function startContainers(request) {
+            return $http.post(app.urlPrefix + 'containers/_start', request);
+        };
 
-		FlotoService.stopContainers = function stopContainers(request) {
-			return $http.post(app.urlPrefix + 'containers/_stop', request);
-		};
+        FlotoService.stopContainers = function stopContainers(request) {
+            return $http.post(app.urlPrefix + 'containers/_stop', request);
+        };
 
-		FlotoService.purgeContainerData = function purgeContainerData(request) {
-			return $http.post(app.urlPrefix + 'containers/_purgeData', request);
-		};
+        FlotoService.purgeContainerData = function purgeContainerData(request) {
+            return $http.post(app.urlPrefix + 'containers/_purgeData', request);
+        };
 
-		FlotoService.getHostStates = function getHostStates() {
-			return $resource(app.urlPrefix + 'hosts/_state').get();
-		};
+        FlotoService.getHostStates = function getHostStates() {
+            return $resource(app.urlPrefix + 'hosts/_state').get();
+        };
 
-		FlotoService.redeployHosts = function redeployHosts(request) {
-			return $http.post(app.urlPrefix + 'hosts/_redeploy', request);
-		};
+        FlotoService.redeployHosts = function redeployHosts(request) {
+            return $http.post(app.urlPrefix + 'hosts/_redeploy', request);
+        };
 
-		FlotoService.startHosts = function startHosts(request) {
-			return $http.post(app.urlPrefix + 'hosts/_start', request);
-		};
+        FlotoService.startHosts = function startHosts(request) {
+            return $http.post(app.urlPrefix + 'hosts/_start', request);
+        };
 
-		FlotoService.stopHosts = function stopHosts(request) {
-			return $http.post(app.urlPrefix + 'hosts/_stop', request);
-		};
+        FlotoService.stopHosts = function stopHosts(request) {
+            return $http.post(app.urlPrefix + 'hosts/_stop', request);
+        };
 
-		FlotoService.deleteHosts = function deleteHosts(request) {
-			return $http.post(app.urlPrefix + 'hosts/_delete', request);
-		};
+        FlotoService.deleteHosts = function deleteHosts(request) {
+            return $http.post(app.urlPrefix + 'hosts/_delete', request);
+        };
 
 
-		FlotoService.getFilePreview = function getFilePreview(url) {
-			var result = {
-				content: "Loading..."
-			};
-			$http.get(app.urlPrefix + url).then(function (response) {
-				result.content = response.data;
-			});
-			return  result;
+        FlotoService.getFilePreview = function getFilePreview(url) {
+            var result = {
+                content: "Loading..."
+            };
+            $http.get(app.urlPrefix + url).then(function (response) {
+                result.content = response.data;
+            });
+            return  result;
 
-		};
+        };
 
-		FlotoService.getContainerTemplates = function getContainerTemplates(containerName) {
+        FlotoService.getContainerTemplates = function getContainerTemplates(containerName) {
 
-			return this.getManifest().$promise.then(function (manifest) {
-				function findImage(imageName) {
-					var image = _.find(manifest.images, {name: imageName});
-					if (!image) {
-						throw "Image " + imageName + " not found";
-					}
-					return image;
-				}
+            return this.getManifest().$promise.then(function (manifest) {
+                function findImage(imageName) {
+                    var image = _.find(manifest.images, {name: imageName});
+                    if (!image) {
+                        throw "Image " + imageName + " not found";
+                    }
+                    return image;
+                }
 
-				function findContainer(containerName) {
-					var container = _.find(manifest.containers, {name: containerName});
-					if (!container) {
-						throw "Container " + containerName + " not found";
-					}
-					return container;
-				}
+                function findContainer(containerName) {
+                    var container = _.find(manifest.containers, {name: containerName});
+                    if (!container) {
+                        throw "Container " + containerName + " not found";
+                    }
+                    return container;
+                }
 
-				var container = findContainer(containerName);
-				var image = findImage(container.image);
-				var templates = [];
-				var steps = image.buildSteps.concat(container.configureSteps);
-				steps.forEach(function (buildStep) {
-					if (buildStep.type === "ADD_TEMPLATE") {
-						templates.push({
-							name: getShortFileName(buildStep.destination),
-							destination: buildStep.destination
-						});
-					}
-				});
+                var container = findContainer(containerName);
+                var image = findImage(container.image);
+                var templates = [];
+                var steps = image.buildSteps.concat(container.configureSteps);
+                steps.forEach(function (buildStep) {
+                    if (buildStep.type === "ADD_TEMPLATE") {
+                        templates.push({
+                            name: getShortFileName(buildStep.destination),
+                            destination: buildStep.destination
+                        });
+                    }
+                });
 
-				return templates;
-			});
+                return templates;
+            });
 
-		};
+        };
 
-		FlotoService.getHostTemplates = function getHostTemplates(hostName) {
+        FlotoService.getHostTemplates = function getHostTemplates(hostName) {
 
-			return this.getManifest().$promise.then(function (manifest) {
-				function findHost(hostName) {
-					var host = _.find(manifest.hosts, {name: hostName});
-					if (!host) {
-						throw "Host " + hostName + " not found";
-					}
-					return host;
-				}
+            return this.getManifest().$promise.then(function (manifest) {
+                function findHost(hostName) {
+                    var host = _.find(manifest.hosts, {name: hostName});
+                    if (!host) {
+                        throw "Host " + hostName + " not found";
+                    }
+                    return host;
+                }
 
-				var host = findHost(hostName);
-				var templates = [];
-				var steps = host.postDeploySteps;
-				steps.forEach(function (buildStep) {
-					if (buildStep.type === "ADD_TEMPLATE") {
-						templates.push({
-							name: getShortFileName(buildStep.destination),
-							destination: buildStep.destination
-						});
-					}
-				});
+                var host = findHost(hostName);
+                var templates = [];
+                var steps = host.postDeploySteps;
+                steps.forEach(function (buildStep) {
+                    if (buildStep.type === "ADD_TEMPLATE") {
+                        templates.push({
+                            name: getShortFileName(buildStep.destination),
+                            destination: buildStep.destination
+                        });
+                    }
+                });
 
-				return templates;
-			});
+                return templates;
+            });
 
-		};
+        };
         FlotoService.refreshManifest();
 
-		return FlotoService;
-	});
+        return FlotoService;
+    });
 
-	function getShortFileName(filename) {
-		return filename.replace(/^.*(\\|\/|\:)/, '');
-	}
+    function getShortFileName(filename) {
+        return filename.replace(/^.*(\\|\/|\:)/, '');
+    }
 })();
