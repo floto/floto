dd914fce018a968f45b09553cf244b7fa860fe05
michas <michas@jarmoni.org>
2014-08-13 22:05:44 +0200

    added support for vbox

--- core/src/main/java/io/github/floto/core/virtualization/virtualbox/VirtualboxHypervisorService.java
+++ core/src/main/java/io/github/floto/core/virtualization/virtualbox/VirtualboxHypervisorService.java
@@ -1,14 +1,9 @@
 package io.github.floto.core.virtualization.virtualbox;
 
-import com.google.common.base.Throwables;
-import com.google.common.net.PercentEscaper;
 import io.github.floto.core.virtualization.HypervisorService;
 import io.github.floto.core.virtualization.VmDescription;
 import io.github.floto.core.virtualization.workstation.ExternalProgram;
 import io.github.floto.dsl.model.VirtualboxHypervisorDescription;
-import org.codehaus.plexus.util.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,9 +13,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Throwables;
+import com.google.common.net.PercentEscaper;
 
 public class VirtualboxHypervisorService implements HypervisorService {
+	
+	public static final int NUM_ATTEMPTS = 9;
+	public static final long SLEEP_INTERVAL = 2000L;
+	
     private Logger log = LoggerFactory.getLogger(VirtualboxHypervisorService.class);
 
     private File cacheDirectory;
@@ -64,25 +75,25 @@ public class VirtualboxHypervisorService implements HypervisorService {
             Throwables.propagate(throwable);
         }
         // Create VM
-        vBoxManage.run("createvm", "--name", desc.vmName, "--register", "--basefolder", vmDirectory.getAbsolutePath());
+        vBoxManage.run("import", cachedFile.getAbsolutePath(), "--vsys", "0", "--vmname", desc.vmName);
 
         // Set VM options
         vBoxManage.run("modifyvm", desc.vmName, "--memory", String.valueOf(desc.memoryInMB), "--cpus", String.valueOf(desc.numberOfCores), "--ostype", "Linux26_64", "--acpi", "on", "--ioapic", "on", "--vram", "32");
 
         // Setup networking
-        vBoxManage.run("modifyvm", desc.vmName, "--nic1", "bridged", "--bridgeadapter1", "eth0");
+        vBoxManage.run("modifyvm", desc.vmName, "--nic1", "hostonly", "--hostonlyadapter1", "vboxnet6");
 
 
         // Create Storage Controller
-        vBoxManage.run("storagectl", desc.vmName, "--name", "sata", "--add", "sata", "--portcount", "4");
+//        vBoxManage.run("storagectl", desc.vmName, "--name", "sata", "--add", "sata", "--portcount", "4");
 
         // Attach ISO
-        vBoxManage.run("storageattach", desc.vmName, "--storagectl", "sata", "--port", "1", "--type", "dvddrive", "--medium", cachedFile.getAbsolutePath());
+//        vBoxManage.run("storageattach", desc.vmName, "--storagectl", "sata", "--port", "1", "--type", "dvddrive", "--medium", cachedFile.getAbsolutePath());
 
         // create and attach disk
-        String diskPath = new File(getVmDirectory(desc.vmName), "disk1.vdi").getAbsolutePath();
-        vBoxManage.run("createhd", "--filename", diskPath, "--format", "vdi", "--size", String.valueOf(20 * 1024));
-        vBoxManage.run("storageattach", desc.vmName, "--storagectl", "sata", "--port", "0", "--type", "hdd", "--medium", diskPath);
+//        String diskPath = new File(getVmDirectory(desc.vmName), "disk1.vdi").getAbsolutePath();
+//        vBoxManage.run("createhd", "--filename", diskPath, "--format", "vdi", "--size", String.valueOf(20 * 1024));
+//        vBoxManage.run("storageattach", desc.vmName, "--storagectl", "sata", "--port", "0", "--type", "hdd", "--medium", diskPath);
     }
 
     private File getVmDirectory(String vmName) {
@@ -219,14 +230,62 @@ public class VirtualboxHypervisorService implements HypervisorService {
             Throwables.propagate(throwable);
         }
     }
-
+    
     @Override
     public void runInVm(String vmname, String cmd) {
-        throw new UnsupportedOperationException("Not implemented");
+    	try {
+	    	this.runCommand(() -> vBoxManage.run("guestcontrol", vmname, "execute", 
+	    			"--username", "user", 
+	    			"--password", "user", 
+	    			"--image", "/usr/bin/sudo",
+	    			"--wait-exit", 
+	    			"--wait-stdout",
+	    			"--wait-stderr",
+	    			"--verbose",
+	    			"--",
+	    			"/bin/bash",
+	    			"-c",
+	    			cmd));
+    	}
+    	catch(Exception ex) {
+    		throw new RuntimeException("Unable to run command " + cmd, ex);
+    	}
     }
 
     @Override
     public void copyFileFromGuest(String vmName, String source, File destination) {
-        throw new UnsupportedOperationException("Not implemented");
+    	try {
+	    	this.runCommand(() -> vBoxManage.run("guestcontrol", vmName, "copyfrom",
+	    			source,
+	    			destination.getAbsolutePath(),
+	    			"--username", "user", 
+	    			"--password", "user", 
+	    			"--verbose"));
+    	}
+    	catch(Exception ex) {
+    		throw new RuntimeException(String.format("Could not copy file from=%s to=%s", source, destination));
+    	}
+    }
+    
+    private void runCommand(Supplier<?> function) {
+    	for(int i = 0; i <= NUM_ATTEMPTS; i++) {
+	    	 try {
+	    		 function.get();
+	    		 return;
+	    	 } catch (Throwable throwable) {
+	    		 if(i < NUM_ATTEMPTS) {
+	    			 try {
+	    				 log.info("guest execution service may not be ready. Will wait a bit...");
+	    				 Thread.sleep(SLEEP_INTERVAL);
+	    			 }
+	    			 catch(InterruptedException iex) {
+	    				 log.warn("Thread interupted");
+	    			 }
+	    		 }
+	    		 else {
+	    			 throw throwable;
+	    		 }
+	         }
+    	}
     }
 }
