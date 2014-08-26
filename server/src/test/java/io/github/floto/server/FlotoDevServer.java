d618ac2eb95cabacf01f1e259ca4638c795418b5
michas <michas@jarmoni.org>
2014-08-26 11:38:08 +0200

    allow setting of definition via argument

--- server/src/test/java/io/github/floto/server/FlotoDevServer.java
+++ server/src/test/java/io/github/floto/server/FlotoDevServer.java
@@ -2,7 +2,15 @@ package io.github.floto.server;
 
 public class FlotoDevServer {
 
-    public static void main(String args[]) {
-        FlotoServer.main(new String[] {"--dev", "--root", "definitions/floto-dev/floto-dev.js"});
-    }
+	public static void main(final String args[]) {
+		String definition = "definitions/floto-dev/floto-dev.js";
+		if(args.length >= 0) {
+			definition = args[0];
+		}
+		FlotoServer
+				.main(new String[] {
+						"--dev",
+						"--root",
+						definition });
+	}
 }
