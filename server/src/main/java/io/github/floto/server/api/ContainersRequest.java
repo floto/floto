b0e98687d2e01727b55535b2803ae5cd3a21b19a
michas <michas@jarmoni.org>
2014-09-25 12:39:00 +0200

    added 'deploymentMode'

--- server/src/main/java/io/github/floto/server/api/ContainersRequest.java
+++ server/src/main/java/io/github/floto/server/api/ContainersRequest.java
@@ -1,8 +1,11 @@
 package io.github.floto.server.api;
 
+import io.github.floto.core.FlotoService.DeploymentMode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContainersRequest {
 	public List<String> containers = new ArrayList<>();
+	public DeploymentMode deploymentMode;
 }
