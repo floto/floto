de5c5791df3cb3c597c24753dc15751f98a268ba
michas <michas@jarmoni.org>
2014-11-29 23:20:24 +0100

    added 3rd interface to vbox-impl to allow communication with outside-world

--- core/src/main/java/io/github/floto/core/virtualization/virtualbox/VirtualboxHypervisorService.java
+++ core/src/main/java/io/github/floto/core/virtualization/virtualbox/VirtualboxHypervisorService.java
@@ -138,8 +138,9 @@ public class VirtualboxHypervisorService implements HypervisorService {
 
 	private void setupNetworking(final VmDescription desc) {
 		// Setup networking
-		vBoxManage.run("modifyvm", desc.vmName, "--nic1", "hostonly", "--hostonlyadapter1", "vboxnet0");
+		vBoxManage.run("modifyvm", desc.vmName, "--nic1", "intnet");
 		vBoxManage.run("modifyvm", desc.vmName, "--nic2", "hostonly", "--hostonlyadapter2", "vboxnet1");
+		vBoxManage.run("modifyvm", desc.vmName, "--nic3", "nat");
 		//vBoxManage.run("modifyvm", desc.vmName, "--nic3", "nat");
 	}
 
