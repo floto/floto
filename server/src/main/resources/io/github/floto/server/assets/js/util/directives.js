43d399d10bd913d8d6c9b9a10e3fbd601f492bfb
Manuel Woelker <github@manuel.woelker.org>
2015-01-07 17:54:45 +0100

    feat(UI/Manifest): faster rendering of large manifests

--- server/src/main/resources/io/github/floto/server/assets/js/util/directives.js
+++ server/src/main/resources/io/github/floto/server/assets/js/util/directives.js
@@ -74,4 +74,116 @@
     });
 
 
+    // break infinite loop when compiling recursive directive (by Mark Lagendijk) - taken from http://stackoverflow.com/a/18609594
+    app.factory('RecursionHelper', ['$compile', function($compile){
+        return {
+            /**
+             * Manually compiles the element, fixing the recursion loop.
+             * @param element
+             * @param [link] A post-link function, or an object with function(s) registered via pre and post properties.
+             * @returns An object containing the linking functions.
+             */
+            compile: function(element, link){
+                // Normalize the link parameter
+                if(angular.isFunction(link)){
+                    link = { post: link };
+                }
+
+                // Break the recursion loop by removing the contents
+                var contents = element.contents().remove();
+                var compiledContents;
+                return {
+                    pre: (link && link.pre) ? link.pre : null,
+                    /**
+                     * Compiles and re-adds the contents
+                     */
+                    post: function(scope, element){
+                        // Compile the contents
+                        if(!compiledContents){
+                            compiledContents = $compile(contents);
+                        }
+                        // Re-add the compiled contents to the element
+                        compiledContents(scope, function(clone){
+                            element.append(clone);
+                        });
+
+                        // Call the post-linking function, if any
+                        if(link && link.post){
+                            link.post.apply(null, arguments);
+                        }
+                    }
+                };
+            }
+        };
+    }]);
+
+    app.directive("jsonStructure", function (RecursionHelper) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                object: "=object"
+            },
+            /*
+            compile: function(element) {
+                // Use the compile function from the RecursionHelper,
+                // And return the linking function(s) which it returns
+                return RecursionHelper.compile(element, {
+                    post: function(scope, element, attr) {
+
+                        scope.typeOf = function(input) {
+                            return typeof input;
+                        }
+                    }
+                });
+            },*/
+            link: function(scope, element, attr) {
+
+                scope.typeOf = function(input) {
+                    return typeof input;
+                }
+            },
+            templateUrl: "js/util/jsonStructure.html"
+        };
+    });
+
+    app.directive("jsonItem", function (RecursionHelper) {
+        return {
+            restrict: 'AE',
+            replace: true,
+            scope: {
+                key: "=",
+                value: "="
+            },
+            compile: function(element) {
+                // Use the compile function from the RecursionHelper,
+                // And return the linking function(s) which it returns
+                return RecursionHelper.compile(element, {
+                    post: function(scope, element, attr) {
+                        scope.isExpanded = false;
+                        scope.isExpandable = false;
+                        scope.isArray = false;
+                        scope.isObject = false;
+                        scope.isSimple = false;
+                        if(Array.isArray(scope.value)) {
+                            scope.isArray = true;
+                            scope.isExpandable = true;
+                        } else if(typeof scope.value === "object") {
+                            scope.isObject = true;
+                            scope.isExpandable = true;
+                        } else {
+                            scope.isSimple = true;
+                        }
+
+                        scope.typeOf = function(input) {
+                            return typeof input;
+                        }
+                    }
+                });
+            },
+            templateUrl: "js/util/jsonItem.html"
+        };
+    });
+
+
 })();
