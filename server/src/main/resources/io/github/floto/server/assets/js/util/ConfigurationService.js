b1ed5082503abd1dd8c62903c9179ec6e4c8302c
Manuel Woelker <github@manuel.woelker.org>
2015-01-23 08:26:13 +0100

    feat(ui): integrated floto-redeploy-button for all containers, container groups and individual containers

--- server/src/main/resources/io/github/floto/server/assets/js/util/ConfigurationService.js
+++ server/src/main/resources/io/github/floto/server/assets/js/util/ConfigurationService.js
@@ -7,7 +7,8 @@
 		configurationFunction = confFn;
 	};
 	var defaultConfiguration = {
-		canRedeploy: true
+		canRedeploy: true,
+        defaultDeploymentMode: "fromBaseImage"
 	};
 	app.factory('configuration', function () {
 		configurationFunction(defaultConfiguration);
@@ -16,5 +17,29 @@
 
 	app.run(function($rootScope, $state, configuration) {
 		$rootScope.configuration = configuration;
+
+        // Initialize deployment modes
+        $rootScope.configuration.deploymentModes = [
+            {
+                name: "fromRootImage",
+                caption: "From Root Image"
+            },
+            {
+                name: "fromBaseImage",
+                caption: "From Base Image"
+            },
+            {
+                name: "containerRebuild",
+                caption: "Recreate container"
+            }
+        ];
+
+        // Mark default deployment mode
+        $rootScope.configuration.deploymentModes.forEach(function(mode) {
+            if(mode.name === $rootScope.configuration.defaultDeploymentMode) {
+                mode.default = true;
+                $rootScope.configuration.defaultDeploymentModeCaption = mode.caption;
+            }
+        })
 	});
 })();
