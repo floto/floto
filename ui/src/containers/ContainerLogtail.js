b9255f8cbdb6039213d35efba91b610f3a359740
Manuel Woelker <github@manuel.woelker.org>
2016-01-28 12:26:59 +0100

    fix(UI): remove newlines from log

--- ui/src/containers/ContainerLogtail.js
+++ ui/src/containers/ContainerLogtail.js
@@ -53,7 +53,7 @@ export default connect(state => {
 			}
 			data.messages.forEach((message) => {
 				var className = "log-" + message.stream;
-				var log = _.escape(message.log);
+				var log = _.escape(message.log).replace(/(?:\r\n|\r|\n)/g, '');;
 				var timestamp = message.time.substr(0, 10) + " " + message.time.substr(11, 8);
 				this.state.messages.push({key: nextId, className, log, timestamp});
 				nextId++;
