7891fb1fa1d5e4dbc72ad379279b06268faddc88
Manuel Woelker <github@manuel.woelker.org>
2016-04-01 11:02:10 +0200

    feat(builder): create documents during build

--- builder/src/main/java/io/github/floto/builder/FlotoBuilder.java
+++ builder/src/main/java/io/github/floto/builder/FlotoBuilder.java
@@ -8,10 +8,12 @@ import io.github.floto.core.FlotoService.DeploymentMode;
 import io.github.floto.core.jobs.ExportVmJob;
 import io.github.floto.core.jobs.RedeployVmJob;
 import io.github.floto.dsl.model.Container;
+import io.github.floto.dsl.model.DocumentDefinition;
 import io.github.floto.dsl.model.Host;
 import io.github.floto.dsl.model.Manifest;
 import io.github.floto.util.task.TaskService;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.joda.time.Duration;
 import org.joda.time.format.PeriodFormat;
@@ -19,6 +21,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.File;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -72,8 +76,25 @@ public class FlotoBuilder {
                 log.error("Expected one host, found: " + manifest.hosts.size());
                 return;
             }
+			Host host = manifest.hosts.get(0);
+			File documentsDirectory = new File(new File("vm/" + host.exportName).getParentFile(), "documents");
 
-            Host host = manifest.hosts.get(0);
+			log.info("Creating documents: {}", documentsDirectory.getAbsolutePath());
+			FileUtils.forceMkdir(documentsDirectory);
+
+			for(DocumentDefinition document: manifest.documents) {
+				String documentString = flotoService.getDocumentString(document.id);
+				String extension = "";
+				String template = document.template;
+				int lastDotIndex = template.lastIndexOf(".");
+				if(lastDotIndex > 0) {
+					extension = template.substring(lastDotIndex);
+				}
+				FileUtils.write(new File(documentsDirectory, document.title + extension), documentString);
+			}
+
+
+
             RedeployVmJob redeployVmJob = new RedeployVmJob(flotoService, host.name);
             redeployVmJob.execute();
 //            flotoService.setExternalHostIp(host.name, "192.168.119.129");
