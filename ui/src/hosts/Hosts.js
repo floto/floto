ce936fce81bc79a10ac8d208a8e523a389b0ca68
github-viktor <github.viktor@icloud.com>
2017-03-17 09:28:03 +0100

    feat(ui/hosts): sort hosts/filter(debounce if >500)

--- ui/src/hosts/Hosts.js
+++ ui/src/hosts/Hosts.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux';
-
 import {Table, Label, Button, SplitButton, MenuItem, DropdownButton, ButtonGroup} from "react-bootstrap";
+import DebounceInput from 'react-debounce-input';
 
 const labelStyleMapping = {
 	running: "success",
 	stopped: "warning",
 	"not-there": "danger"
+};
+
+let hostSortTypes = {
+	none: {title: "Deployment order", sortFn: (hosts) =>  hosts },
+	name: {title: "Name", sortFn: (hosts) => _.sortBy(hosts, "name") }
 };
 
 export default connect(state => {
@@ -16,6 +21,7 @@ export default connect(state => {
 	return {
 		clientState: state.clientState,
 		hosts: hosts,
+		hostFilter: state.hostFilter,
 		selectedHost: state.selectedHost
 	};
 })(React.createClass({
@@ -33,6 +39,26 @@ export default connect(state => {
 					newUrl = this.props.location.pathname.replace(currentUrl, newUrl);
 				}
 				this.context.router.push({pathname: newUrl, query: this.props.location.query});
+			},
+
+			onChangeHostsSort(event, sort) {
+				let query = this.props.location.query || {};
+				if (sort === "none") {
+					delete query.sort;
+				}
+				else {
+					query.sort = sort;
+				}
+				this.context.router.push({pathname: this.props.location.pathname, query: query});
+			},
+
+			onChangeHostFilter(event) {
+				let query = this.props.location.query || {};
+				query.hostFilter = event.target.value.trim();
+				if(query.hostFilter === "") {
+					delete query.hostFilter;
+				}
+				this.context.router.push({pathname: this.props.location.pathname, query: query});
 			},
 
 			renderHost(host) {
@@ -66,18 +92,76 @@ export default connect(state => {
 			render() {
 				let actions = this.context.actions;
 				let hosts = this.props.hosts || [];
+				let allHosts = hosts;
 				let selectedHost = this.selectedHost = this.props.selectedHost || {};
 				let safetyArmed = this.safetyArmed = this.props.clientState.safetyArmed;
-				let allHostNames = _.pluck(hosts, "name");
+				let query = (this.props.location.query || {});
+				let hostSortKey = query.sort || "none";
+				let hostSortType = hostSortTypes[hostSortKey] || hostSortTypes.none;
+				hosts = hostSortType.sortFn(hosts);
+
+				let hostFilter = query.hostFilter || "";
+				let hostFilterError = null;
+				let hostFilterRegex = null;
+				try {
+					hostFilterRegex = new RegExp(hostFilter, "i");
+				} catch(error) {
+					console.log(error);
+					hostFilterError = ""+error;
+				}
+
+				if(hostFilterRegex !== null) {
+					hosts = _.filter(hosts, (host) => hostFilterRegex.test(host.name));
+				}
+
+				//patch-maker always at first position
+				let patchMaker = _.find(hosts, (host) => host.name == "patch-maker");
+				hosts = _.filter(hosts, (host) => host.name != "patch-maker");
+				if (patchMaker != null) hosts.splice(0, 0, patchMaker);
+
+				let hostNames = _.pluck(hosts, "name");
+
+				let hostsCountName = "all";
+				if(hosts.length != allHosts.length) {
+					hostsCountName = hosts.length;
+				}
+
+				let buttonStyle = {width: "100px"};
 				return <div style={{height: "100%"}}>
 					<div style={{display: "flex", flexboxDirection: "row", flexWrap: "nowrap", height: "100%"}}>
 						<div style={{flex: 1, height: "100%", display:"flex", flexDirection: "column"}}>
 							<div style={{flex: "0 0 auto", marginBottom: "10px"}}>
-								<h2>Hosts <span className="text-muted">({hosts.length})</span></h2>
+								<h2>Hosts <span className="text-muted">({hosts.length})</span>
+								<span className="pull-right"><small>Sort by:&nbsp;&nbsp;&nbsp;</small>
+									<DropdownButton bsStyle="default" title={hostSortType.title}
+													id="hosts-sort"
+													onSelect={this.onChangeHostsSort}>
+										{_.map(hostSortTypes, (sortType, key) =>
+											<MenuItem key={key} eventKey={key}><span
+												style={{fontWeight: key === hostSortKey ? "bold": "normal"}}>{sortType.title}</span></MenuItem>)}
+									</DropdownButton>
+								</span>
+								</h2>
 								<ButtonGroup>
 									<Button onClick={actions.loadHostStates}>Refresh</Button>
-									<Button bsStyle="primary" onClick={() => actions.redeployHosts(allHostNames)}
-											disabled={!safetyArmed}>Redeploy all</Button>
+									<Button bsStyle="primary" onClick={() => actions.redeployHosts(hostNames)}
+											disabled={!safetyArmed}>{"Redeploy "+hostsCountName+" hosts"}</Button>
+									<Button bsStyle="success" onClick={() => actions.startHosts(hostNames)}
+											disabled={!safetyArmed} style={buttonStyle} >Start {hostsCountName}</Button>
+									<Button bsStyle="danger" onClick={() => actions.stopHosts(hostNames.slice().reverse())}
+											disabled={!safetyArmed} style={buttonStyle}>Stop {hostsCountName}</Button>
+									<span className={hostFilterError?"has-warning":""}>
+									<DebounceInput
+										style={{display: "inline-block", width: "160px", marginLeft: "5px"}}
+										type="text"
+										placeholder="Filter hosts"
+										title={hostFilterError}
+										value={hostFilter}
+										debounceTimeout={(allHosts.length > 500)? 500 : 0}
+										onChange={this.onChangeHostFilter}
+										className="form-control"
+									/>
+									</span>
 								</ButtonGroup>
 							</div>
 							<div style={{flex: "1 1 auto", overflowY: "scroll"}}>
