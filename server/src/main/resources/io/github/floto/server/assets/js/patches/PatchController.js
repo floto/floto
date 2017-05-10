76b416b50138ed3875e895ab629f0a919c47e453
github-viktor <github.viktor@icloud.com>
2017-05-10 15:42:43 +0200

    chore(ui): remove old angular ui

--- server/src/main/resources/io/github/floto/server/assets/js/patches/PatchController.js
+++
@@ -1,8 +0,0 @@
-(function () {
-
-    app.controller("PatchController", function ($scope, FlotoService, $stateParams, TaskService, NotificationService) {
-        var patchId = $stateParams["patchId"];
-        $scope.patchId = patchId;
-        $scope.patch = FlotoService.getPatchInfo(patchId);
-    });
-})();
