ebd5d28ef6b6b89239340a27e9c0a15001ca065a
MF <marco-fehrenbach@users.noreply.github.com>
2017-05-12 15:32:50 +0200

    chore(ui): Update RedeployButton for React 15.5.4.

--- ui/src/components/RedeployButton.js
+++ ui/src/components/RedeployButton.js
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux';
+import {SplitButton, MenuItem} from "react-bootstrap";
+import React from 'react';
 
-import {Table, Label, Button, SplitButton, MenuItem} from "react-bootstrap";
+class RedeployButton extends React.Component {
 
-export default connect(state => {
-	return {config: state.config};
-})(React.createClass({
 	onExecute(deploymentMode, event) {
 		event.stopPropagation();
 		this.props.onExecute(deploymentMode);
-	},
+	}
 
 	render() {
 		let config = this.props.config;
@@ -19,14 +18,18 @@ export default connect(state => {
 		let rootStyle = defaultFromBase ? null : {fontWeight: "bold"};
 		let baseStyle = defaultFromBase ? {fontWeight: "bold"} : null;
 
-		return <SplitButton bsStyle={this.props.bsStyle||"primary"} bsSize={this.props.size}
+		return <SplitButton bsStyle={this.props.bsStyle || "primary"} bsSize={this.props.size}
 							onClick={this.onExecute.bind(this, defaultDeploymentMode)} style={this.props.style}
 							title={this.props.title || "Redeploy"} id="redeploy" disabled={this.props.disabled}>
-			{config.canDeployFromRootImage?<MenuItem onSelect={this.onExecute.bind(this, "fromRootImage")}><span
-				style={rootStyle}>From Root Image</span></MenuItem>:null}
+			{config.canDeployFromRootImage ? <MenuItem onSelect={this.onExecute.bind(this, "fromRootImage")}><span
+					style={rootStyle}>From Root Image</span></MenuItem> : null}
 			<MenuItem onSelect={this.onExecute.bind(this, "fromBaseImage")}><span
 				style={baseStyle}>From Base Image</span></MenuItem>
 			<MenuItem onSelect={this.onExecute.bind(this, "containerRebuild")}>Recreate Container</MenuItem>
 		</SplitButton>;
 	}
-}));
+}
+
+export default connect(state => {
+	return {config: state.config};
+})(RedeployButton);
