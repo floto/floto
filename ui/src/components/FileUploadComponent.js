931a1935ff72dfbdd9a8ebfbc0fe3b5313f474f8
MF <marco-fehrenbach@users.noreply.github.com>
2017-05-17 16:42:19 +0200

    chore(ui): Update FileUploadComponent for React 15.5.4.

--- ui/src/components/FileUploadComponent.js
+++ ui/src/components/FileUploadComponent.js
@@ -1,13 +1,22 @@
-import {Table, Label, Button, SplitButton, MenuItem, DropdownButton, ButtonGroup} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class FileUploadComponent extends React.Component {
 
-export default React.createClass({
+	constructor() {
+		super();
+
+		this.uploadFile = this.uploadFile.bind(this);
+		this.onFileSelected = this.onFileSelected.bind(this);
+	}
 
 	uploadFile(event) {
 		event.preventDefault();
 		var inputNode = ReactDOM.findDOMNode(this.refs.fileUpload);
 		inputNode.click();
-	},
+	}
+
 	onFileSelected() {
 		var inputNode = ReactDOM.findDOMNode(this.refs.fileUpload);
 		if(inputNode.files && inputNode.files[0]) {
@@ -15,14 +24,16 @@ export default React.createClass({
 			this.props.onFileSelected(file);
 		}
 		ReactDOM.findDOMNode(this.refs.form).reset();
-	},
+	}
 
 	render() {
 		return <div>
 			<Button bsStyle="primary" onClick={this.uploadFile}>{this.props.title}</Button>
 			<form style={{display: "none"}} ref="form"><input ref="fileUpload" type="file"
-																 onChange={this.onFileSelected}/></form>
+															  onChange={this.onFileSelected}/></form>
 		</div>;
 	}
-});
+}
+
+export default FileUploadComponent;
 
