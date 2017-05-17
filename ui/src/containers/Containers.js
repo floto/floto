d2fe22b48eac5254b0617b58ee646a9a73cfacdc
MF <marco-fehrenbach@users.noreply.github.com>
2017-05-17 16:33:53 +0200

    chore(ui): Update Containers for React 15.5.4.

--- ui/src/containers/Containers.js
+++ ui/src/containers/Containers.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux';
 import DebounceInput from 'react-debounce-input';
-
 import ContainerGroup from "./ContainerGroup.js";
-
-import {Table, Label, Button, SplitButton, MenuItem, DropdownButton, ButtonGroup, Panel, Input} from "react-bootstrap";
-
+import React from 'react';
+import {Table, Label, Button, MenuItem, DropdownButton, ButtonGroup, Panel, Input} from "react-bootstrap";
 import RedeployButton from "../components/RedeployButton.js";
 
 let containerGroupings = {
@@ -33,163 +31,156 @@ let containerGroupings = {
 	}
 };
 
-export default connect(state => {
-	return {
-		manifestError: state.manifestError,
-		clientState: state.clientState,
-		containers: state.containers,
-		containerFilter: state.containerFilter,
-		unmanagedContainers: state.unmanagedContainers,
-		selectedContainer: state.selectedContainer
-	};
-})(React.createClass({
-		displayName: "Containers",
-		contextTypes: {
-			actions: React.PropTypes.object.isRequired,
-			router: React.PropTypes.object.isRequired
-		},
+class Containers extends React.Component {
 
-		onChangeContainerGrouping(grouping, event) {
-			let query = this.props.location.query || {};
-			if (grouping === "none") {
-				delete query.grouping;
-			}
-			else {
-				query.grouping = grouping;
-			}
-			this.context.router.push({pathname: this.props.location.pathname, query: query});
-		},
+	constructor() {
+		super();
 
-		onChangeContainerFilter(event) {
-			let query = this.props.location.query || {};
-			query.containerFilter = event.target.value.trim();
-			if(query.containerFilter === "") {
-				delete query.containerFilter;
-			}
-			this.context.router.push({pathname: this.props.location.pathname, query: query});
-		},
+		this.onChangeContainerFilter = this.onChangeContainerFilter.bind(this);
+		this.onChangeContainerGrouping = this.onChangeContainerGrouping.bind(this);
+	}
 
-		render() {
-			let actions = this.context.actions;
-			let containers = this.props.containers || [];
-			let safetyArmed = this.safetyArmed = this.props.clientState.safetyArmed;
-			let query = (this.props.location.query || {});
-            let containerGroupingKey = query.grouping || "none";
-			let containerFilter = query.containerFilter || "";
-			let containerGrouping = containerGroupings[containerGroupingKey] || containerGroupings.none;
-			let allContainerNames = _.map(containers, (container) => container.name);
-			let groups = containerGrouping.groupFn(containers);
-			let unmanagedContainers = this.props.unmanagedContainers;
-			groups = _.sortBy(groups, "title");
-			let unmanagedContainersComponent = null;
-			let selectedContainer = this.props.selectedContainer || {};
-			if (containerGroupingKey === "none" && unmanagedContainers && unmanagedContainers.length > 0) {
-				groups[0].title = "Managed containers";
-				unmanagedContainersComponent = <div>
-					<h4>Unmanaged Containers<span className="text-muted"> ({unmanagedContainers.length})</span></h4>
-					<Table bordered striped hover condensed style={{cursor: "pointer"}}>
-						<tbody>
-						{unmanagedContainers.map((container) => <tr key={container.name} className="warning">
-							<td><Label bsStyle="warning">{container.state.status || "unknown" }</Label></td>
-							<td><Button bsStyle="danger" bsSize="xs"
-										onClick={actions.destroyContainers.bind(null, container.name, container.state.hostName)}>Destroy</Button>
-							</td>
-							<td style={{width: "100%"}}>{container.name}@{container.state.hostName}</td>
-						</tr>)}
-						</tbody>
-					</Table>
-				</div>;
-			}
-			let containerFilterError = null;
-			let containerFilterRegex = null;
-			try {
-				containerFilterRegex = new RegExp(containerFilter, "i");
-			} catch(error) {
-				console.log(error);
-				containerFilterError = ""+error;
-			}
+	onChangeContainerGrouping(grouping, event) {
+		let query = this.props.location.query || {};
+		if (grouping === "none") {
+			delete query.grouping;
+		}
+		else {
+			query.grouping = grouping;
+		}
+		this.context.router.push({pathname: this.props.location.pathname, query: query});
+	}
 
-			let filteredContainerCount = 0;
-			let filteredContainerNames = [];
-			let controlledContainerNames = [];
-			let changedContainerNames = [];
-			let startableContainerNames = [];
-			let stoppableContainerNames = [];
-			
-			_.forEach(groups, group => {
-				
-				let groupControlledContainerNames = [];
-				let groupStartableContainerNames = [];
-				let groupStoppableContainerNames = [];
-				
-				group.containers = _.sortBy(group.containers, "name");
-				group.totalCount = group.containers.length;
-				if(containerFilterRegex !== null) {
-					group.containers = _.filter(group.containers, (container) => containerFilterRegex.test(container.name));
+	onChangeContainerFilter(event) {
+		let query = this.props.location.query || {};
+		query.containerFilter = event.target.value.trim();
+		if(query.containerFilter === "") {
+			delete query.containerFilter;
+		}
+		this.context.router.push({pathname: this.props.location.pathname, query: query});
+	}
+
+	render() {
+		let actions = this.context.actions;
+		let containers = this.props.containers || [];
+		let safetyArmed = this.safetyArmed = this.props.clientState.safetyArmed;
+		let query = (this.props.location.query || {});
+		let containerGroupingKey = query.grouping || "none";
+		let containerFilter = query.containerFilter || "";
+		let containerGrouping = containerGroupings[containerGroupingKey] || containerGroupings.none;
+		let allContainerNames = _.map(containers, (container) => container.name);
+		let groups = containerGrouping.groupFn(containers);
+		let unmanagedContainers = this.props.unmanagedContainers;
+		groups = _.sortBy(groups, "title");
+		let unmanagedContainersComponent = null;
+		let selectedContainer = this.props.selectedContainer || {};
+		if (containerGroupingKey === "none" && unmanagedContainers && unmanagedContainers.length > 0) {
+			groups[0].title = "Managed containers";
+			unmanagedContainersComponent = <div>
+				<h4>Unmanaged Containers<span className="text-muted"> ({unmanagedContainers.length})</span></h4>
+				<Table bordered striped hover condensed style={{cursor: "pointer"}}>
+					<tbody>
+					{unmanagedContainers.map((container) => <tr key={container.name} className="warning">
+						<td><Label bsStyle="warning">{container.state.status || "unknown" }</Label></td>
+						<td><Button bsStyle="danger" bsSize="xs"
+									onClick={actions.destroyContainers.bind(null, container.name, container.state.hostName)}>Destroy</Button>
+						</td>
+						<td style={{width: "100%"}}>{container.name}@{container.state.hostName}</td>
+					</tr>)}
+					</tbody>
+				</Table>
+			</div>;
+		}
+		let containerFilterError = null;
+		let containerFilterRegex = null;
+		try {
+			containerFilterRegex = new RegExp(containerFilter, "i");
+		} catch(error) {
+			console.log(error);
+			containerFilterError = ""+error;
+		}
+
+		let filteredContainerCount = 0;
+		let filteredContainerNames = [];
+		let controlledContainerNames = [];
+		let changedContainerNames = [];
+		let startableContainerNames = [];
+		let stoppableContainerNames = [];
+
+		_.forEach(groups, group => {
+
+			let groupControlledContainerNames = [];
+			let groupStartableContainerNames = [];
+			let groupStoppableContainerNames = [];
+
+			group.containers = _.sortBy(group.containers, "name");
+			group.totalCount = group.containers.length;
+			if(containerFilterRegex !== null) {
+				group.containers = _.filter(group.containers, (container) => containerFilterRegex.test(container.name));
+			}
+			group.changedContainerNames = [];
+			_.forEach(group.containers, (container) => {
+				if (!container.externalContainer) {
+					controlledContainerNames.push(container.name);
+					groupControlledContainerNames.push(container.name);
+					if (container.state.needsRedeploy) {
+						changedContainerNames.push(container.name);
+						group.changedContainerNames.push(container.name);
+					}
 				}
-				group.changedContainerNames = [];
-				_.forEach(group.containers, (container) => {
-					if (!container.externalContainer) {
-						controlledContainerNames.push(container.name);
-						groupControlledContainerNames.push(container.name);
-						if (container.state.needsRedeploy) {
-							changedContainerNames.push(container.name);
-							group.changedContainerNames.push(container.name);
-						}
-					}
-					if(container.startable == null || container.startable == true){
-						startableContainerNames.push(container.name);
-						groupStartableContainerNames.push(container.name);
-					}
-					if(container.stoppable == null || container.stoppable == true){
-						stoppableContainerNames.push(container.name);
-						groupStoppableContainerNames.push(container.name);
-					}
-				});
-				group.containerNames = _.map(group.containers, (container) => container.name);
-				group.controlledContainerNames = groupControlledContainerNames;
-				group.startableContainerNames = groupStartableContainerNames;
-				group.stoppableContainerNames = groupStoppableContainerNames;
+				if(container.startable == null || container.startable == true){
+					startableContainerNames.push(container.name);
+					groupStartableContainerNames.push(container.name);
+				}
+				if(container.stoppable == null || container.stoppable == true){
+					stoppableContainerNames.push(container.name);
+					groupStoppableContainerNames.push(container.name);
+				}
+			});
+			group.containerNames = _.map(group.containers, (container) => container.name);
+			group.controlledContainerNames = groupControlledContainerNames;
+			group.startableContainerNames = groupStartableContainerNames;
+			group.stoppableContainerNames = groupStoppableContainerNames;
 			filteredContainerNames = filteredContainerNames.concat(group.containerNames);
 			filteredContainerCount += group.containers.length;
-			});
+		});
 
-			groups = _.filter(groups, (group) => group.containers.length > 0);
+		groups = _.filter(groups, (group) => group.containers.length > 0);
 
 
-			if (this.props.manifestError) {
-				return <div style={{height: "100%", marginTop: 20, marginRight: 20}}>
-					<Panel header="Manifest compilation error" bsStyle="danger">
-						{this.props.manifestError.message}
-					</Panel>
-				</div>;
-			}
-			let containerCount = containers.length;
-			let containerCountName = "all";
-			let controlledCountName = "all";
-			let startableCountName = "all";
-			let stoppableCountName = "all";
-			if(filteredContainerCount !== containers.length) {
-                containerCount = filteredContainerCount  + "/" + containerCount;
-                containerCountName = filteredContainerCount;
-            }
-			if(controlledContainerNames.length !== filteredContainerCount) {
-				controlledCountName = controlledContainerNames.length;
-			}
-			if(startableContainerNames.length !== filteredContainerCount) {
-				startableCountName = startableContainerNames.length;
-			}
-			if(stoppableContainerNames.length !== filteredContainerCount) {
-				stoppableCountName = stoppableContainerNames.length;
-			}
+		if (this.props.manifestError) {
+			return <div style={{height: "100%", marginTop: 20, marginRight: 20}}>
+				<Panel header="Manifest compilation error" bsStyle="danger">
+					{this.props.manifestError.message}
+				</Panel>
+			</div>;
+		}
+		let containerCount = containers.length;
+		let containerCountName = "all";
+		let controlledCountName = "all";
+		let startableCountName = "all";
+		let stoppableCountName = "all";
+		if(filteredContainerCount !== containers.length) {
+			containerCount = filteredContainerCount  + "/" + containerCount;
+			containerCountName = filteredContainerCount;
+		}
+		if(controlledContainerNames.length !== filteredContainerCount) {
+			controlledCountName = controlledContainerNames.length;
+		}
+		if(startableContainerNames.length !== filteredContainerCount) {
+			startableCountName = startableContainerNames.length;
+		}
+		if(stoppableContainerNames.length !== filteredContainerCount) {
+			stoppableCountName = stoppableContainerNames.length;
+		}
 
-			let buttonStyle = {width: "100px"};
-			return <div style={{height: "100%"}}>
-				<div style={{display: "flex", flexboxDirection: "row", flexWrap: "nowrap", height: "100%"}}>
-					<div style={{flex: "1 1 auto", width: "50%", height: "100%", display:"flex", flexDirection: "column"}}>
-						<div style={{flex: "0 0 auto", marginBottom: "10px", marginRight: "10px"}}>
-							<h2>Containers <span className="text-muted">({containerCount})</span>
-																<span className="pull-right"><small>Grouping:&nbsp;&nbsp;&nbsp;</small>
+		let buttonStyle = {width: "100px"};
+		return <div style={{height: "100%"}}>
+			<div style={{display: "flex", flexboxDirection: "row", flexWrap: "nowrap", height: "100%"}}>
+				<div style={{flex: "1 1 auto", width: "50%", height: "100%", display:"flex", flexDirection: "column"}}>
+					<div style={{flex: "0 0 auto", marginBottom: "10px", marginRight: "10px"}}>
+						<h2>Containers <span className="text-muted">({containerCount})</span>
+							<span className="pull-right"><small>Grouping:&nbsp;&nbsp;&nbsp;</small>
 									<DropdownButton bsStyle="default" title={containerGrouping.title}
 													id="container-grouping"
 													onSelect={this.onChangeContainerGrouping}>
@@ -199,18 +190,18 @@ export default connect(state => {
 									</DropdownButton>
 								</span>
 
-							</h2>
-							<ButtonGroup>
-								<Button onClick={actions.loadContainerStates}>Refresh</Button>
-								<RedeployButton style={{width: "150px"}} disabled={!safetyArmed || changedContainerNames.length < 1} title={"Redeploy " + changedContainerNames.length + " changed"}
-												onExecute={(deploymentMode) => actions.redeployContainers( changedContainerNames, deploymentMode)}/>
-								<RedeployButton style={buttonStyle} disabled={!safetyArmed || controlledContainerNames.length < 1} title={"Redeploy " + controlledCountName}
-												onExecute={(deploymentMode) => actions.redeployContainers( controlledContainerNames, deploymentMode)}/>
-								<Button bsStyle="success" onClick={() => actions.startContainers(startableContainerNames)}
-										disabled={!safetyArmed || startableContainerNames.length < 1} style={buttonStyle} >Start {startableCountName}</Button>
-								<Button bsStyle="danger" onClick={() => actions.stopContainers(stoppableContainerNames)}
-										disabled={!safetyArmed || stoppableContainerNames.length < 1} style={buttonStyle}>Stop {stoppableCountName}</Button>
-								<span className={containerFilterError?"has-warning":""}>
+						</h2>
+						<ButtonGroup>
+							<Button onClick={actions.loadContainerStates}>Refresh</Button>
+							<RedeployButton style={{width: "150px"}} disabled={!safetyArmed || changedContainerNames.length < 1} title={"Redeploy " + changedContainerNames.length + " changed"}
+											onExecute={(deploymentMode) => actions.redeployContainers( changedContainerNames, deploymentMode)}/>
+							<RedeployButton style={buttonStyle} disabled={!safetyArmed || controlledContainerNames.length < 1} title={"Redeploy " + controlledCountName}
+											onExecute={(deploymentMode) => actions.redeployContainers( controlledContainerNames, deploymentMode)}/>
+							<Button bsStyle="success" onClick={() => actions.startContainers(startableContainerNames)}
+									disabled={!safetyArmed || startableContainerNames.length < 1} style={buttonStyle} >Start {startableCountName}</Button>
+							<Button bsStyle="danger" onClick={() => actions.stopContainers(stoppableContainerNames)}
+									disabled={!safetyArmed || stoppableContainerNames.length < 1} style={buttonStyle}>Stop {stoppableCountName}</Button>
+							<span className={containerFilterError?"has-warning":""}>
 								<DebounceInput
 									style={{display: "inline-block", width: "160px", marginLeft: "5px"}}
 									type="text"
@@ -222,26 +213,39 @@ export default connect(state => {
 									className="form-control"
 								/>
 								</span>
-							</ButtonGroup>
-						</div>
-						<div style={{flex: "1 1 auto", overflowY: "scroll"}}>
-							{unmanagedContainersComponent}
-							{groups.map((group) =>
-								<ContainerGroup key={group.title || group.id} group={group}
-												location={this.props.location}/>)}
-						</div>
+						</ButtonGroup>
 					</div>
-					<div key={selectedContainer.name}
-						 style={{flex: "1 1 auto", width: "50%", paddingLeft: 20, height: "100%"}}>
-						{this.props.children}
+					<div style={{flex: "1 1 auto", overflowY: "scroll"}}>
+						{unmanagedContainersComponent}
+						{groups.map((group) =>
+							<ContainerGroup key={group.title || group.id} group={group}
+											location={this.props.location}/>)}
 					</div>
 				</div>
-			</div>;
-
-		}
+				<div key={selectedContainer.name}
+					 style={{flex: "1 1 auto", width: "50%", paddingLeft: 20, height: "100%"}}>
+					{this.props.children}
+				</div>
+			</div>
+		</div>;
 	}
-	)
-);
+}
+
+Containers.contextTypes = {
+	actions: React.PropTypes.object.isRequired,
+	router: React.PropTypes.object.isRequired
+};
+
+export default connect(state => {
+	return {
+		manifestError: state.manifestError,
+		clientState: state.clientState,
+		containers: state.containers,
+		containerFilter: state.containerFilter,
+		unmanagedContainers: state.unmanagedContainers,
+		selectedContainer: state.selectedContainer
+	};
+})(Containers);
 
 
 
