2da93a79aa296a7c1da222df3fa4762282547400
MF <marco-fehrenbach@users.noreply.github.com>
2017-05-18 16:28:36 +0200

    chore(ui): Update Patches for React 15.5.4.

--- ui/src/patches/Patches.js
+++ ui/src/patches/Patches.js
@@ -1,10 +1,100 @@
 import { connect } from 'react-redux';
 
-import {Table, Label, Button, SplitButton, MenuItem, DropdownButton, ButtonGroup} from "react-bootstrap";
+import {Table, Button} from "react-bootstrap";
 import Icon from 'react-fa';
-
 import FileUploadComponent from "../components/FileUploadComponent.js";
+import React from 'react';
 
+class Patches extends React.Component {
+
+	constructor() {
+		super();
+
+		this.navigateToPatch = this.navigateToPatch.bind(this);
+		this.renderPatch = this.renderPatch.bind(this);
+	}
+
+	navigateToPatch(patchId) {
+		let newUrl = '/patches/' + patchId;
+		this.context.router.push({pathname: newUrl, query: this.props.location.query});
+	}
+
+	renderPatch(patch) {
+		let actions = this.context.actions;
+		let rowClassName = null;
+		let style = null;
+		if (patch.id === this.activePatchId) {
+			rowClassName = "warning";
+			style = {fontWeight: "bold"};
+		}
+		if (patch.id === this.selectedPatchId) {
+			rowClassName = "info";
+		}
+		return <tr key={patch.id} onClick={this.navigateToPatch.bind(this, patch.id)}
+				   className={rowClassName} style={style}>
+			<td>{patch.creationDate}</td>
+			<td>{patch.name ?
+				<span>{patch.name} <span className="text-muted">({patch.revision})</span></span>
+				: patch.revision}</td>
+			<td>{patch.parentName ?
+				<span>{patch.parentName} <span className="text-muted">({patch.parentRevision})</span></span>
+				: patch.parentRevision || "-"}</td>
+		</tr>;
+	}
+
+	render() {
+		let actions = this.context.actions;
+		let config = this.props.config;
+		let patches = this.props.patches || [];
+		let selectedPatchId = this.selectedPatchId = this.props.selectedPatchId;
+		this.activePatchId = this.props.activePatchId;
+		return <div style={{height: "100%"}}>
+			<div style={{display: "flex", flexboxDirection: "row", flexWrap: "nowrap", height: "100%"}}>
+				<div style={{flex: 1, height: "100%", display:"flex", flexDirection: "column"}}>
+					<div style={{flex: "0 0 auto", marginBottom: "10px"}}>
+						<h2>Patches <span className="text-muted">({patches.length})</span></h2>
+						<Button onClick={actions.loadPatches}>Refresh</Button>
+						<span className="pull-right">
+										{this.props.uploadProgress?<span>Uploading...{this.props.uploadProgress.percentComplete}%</span>:
+											<FileUploadComponent title={<span><Icon name="upload" />&nbsp;&nbsp;Upload patch</span>} extension=".floto-patch.zip"
+																 onFileSelected={(patchFile) => actions.uploadPatch(patchFile)}/>}
+								</span>
+						{config.patchMode === "create" ?
+							<span className="pull-right">
+									<Button bsStyle="primary"
+											onClick={() => actions.createFullPatch()}>Create
+										full patch</Button>
+								</span> : null}
+
+					</div>
+					<div style={{flex: "1 1 auto", overflowY: "scroll"}}>
+						<Table bordered striped hover condensed style={{cursor: "pointer"}}>
+							<thead>
+							<tr>
+								<th style={{width: "12em"}}>Created</th>
+								<th>Revision</th>
+								<th>Parent</th>
+							</tr>
+							</thead>
+							<tbody>
+							{patches.map(this.renderPatch)}
+							</tbody>
+						</Table>
+					</div>
+				</div>
+				<div key={selectedPatchId} style={{flex: 1, paddingLeft: 20, height: "100%"}}>
+					{this.props.children}
+				</div>
+			</div>
+		</div>;
+
+	}
+}
+
+Patches.contextTypes = {
+	actions: React.PropTypes.object.isRequired,
+	router: React.PropTypes.object.isRequired
+}
 
 export default connect(state => {
 	return {
@@ -15,89 +105,5 @@ export default connect(state => {
 		uploadProgress: state.uploadProgress,
 		config: state.config
 	};
-})(React.createClass({
-			displayName: "Patches",
-			contextTypes: {
-				actions: React.PropTypes.object.isRequired,
-				router: React.PropTypes.object.isRequired
-			},
-
-			navigateToPatch(patchId) {
-				let newUrl = '/patches/' + patchId;
-				this.context.router.push({pathname: newUrl, query: this.props.location.query});
-			},
-
-			renderPatch(patch) {
-				let actions = this.context.actions;
-				let rowClassName = null;
-				let style = null;
-				if (patch.id === this.activePatchId) {
-					rowClassName = "warning";
-					style = {fontWeight: "bold"};
-				}
-				if (patch.id === this.selectedPatchId) {
-					rowClassName = "info";
-				}
-				return <tr key={patch.id} onClick={this.navigateToPatch.bind(this, patch.id)}
-						   className={rowClassName} style={style}>
-					<td>{patch.creationDate}</td>
-					<td>{patch.name ?
-						<span>{patch.name} <span className="text-muted">({patch.revision})</span></span>
-						: patch.revision}</td>
-					<td>{patch.parentName ?
-						<span>{patch.parentName} <span className="text-muted">({patch.parentRevision})</span></span>
-						: patch.parentRevision || "-"}</td>
-				</tr>;
-			},
-
-			render() {
-				let actions = this.context.actions;
-				let config = this.props.config;
-				let patches = this.props.patches || [];
-				let selectedPatchId = this.selectedPatchId = this.props.selectedPatchId;
-				this.activePatchId = this.props.activePatchId;
-				return <div style={{height: "100%"}}>
-					<div style={{display: "flex", flexboxDirection: "row", flexWrap: "nowrap", height: "100%"}}>
-						<div style={{flex: 1, height: "100%", display:"flex", flexDirection: "column"}}>
-							<div style={{flex: "0 0 auto", marginBottom: "10px"}}>
-								<h2>Patches <span className="text-muted">({patches.length})</span></h2>
-								<Button onClick={actions.loadPatches}>Refresh</Button>
-									<span className="pull-right">
-										{this.props.uploadProgress?<span>Uploading...{this.props.uploadProgress.percentComplete}%</span>:
-										<FileUploadComponent title={<span><Icon name="upload" />&nbsp;&nbsp;Upload patch</span>} extension=".floto-patch.zip"
-															 onFileSelected={(patchFile) => actions.uploadPatch(patchFile)}/>}
-								</span>
-								{config.patchMode === "create" ?
-									<span className="pull-right">
-									<Button bsStyle="primary"
-											onClick={() => actions.createFullPatch()}>Create
-										full patch</Button>
-								</span> : null}
-
-							</div>
-							<div style={{flex: "1 1 auto", overflowY: "scroll"}}>
-								<Table bordered striped hover condensed style={{cursor: "pointer"}}>
-									<thead>
-									<tr>
-										<th style={{width: "12em"}}>Created</th>
-										<th>Revision</th>
-										<th>Parent</th>
-									</tr>
-									</thead>
-									<tbody>
-									{patches.map(this.renderPatch)}
-									</tbody>
-								</Table>
-							</div>
-						</div>
-						<div key={selectedPatchId} style={{flex: 1, paddingLeft: 20, height: "100%"}}>
-							{this.props.children}
-						</div>
-					</div>
-				</div>;
-
-			}
-		}
-	)
-);
+})(Patches);
 
