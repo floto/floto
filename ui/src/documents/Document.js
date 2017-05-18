7fd9d38fe4b58e3386873e3137b5b7fa454887af
MF <marco-fehrenbach@users.noreply.github.com>
2017-05-18 13:53:19 +0200

    chore(ui): Update Document for React 15.5.4.

--- ui/src/documents/Document.js
+++ ui/src/documents/Document.js
@@ -1,31 +1,26 @@
 import { connect } from 'react-redux';
+import React from 'react';
 
-import { Navigation } from 'react-router';
+class Document extends React.Component {
+	render() {
+		var selectedDocument = this.props.selectedDocument;
+		return <div style={{height: "100%", width: "100%"}}>
+			<iframe style={{height: "100%", width: "100%", border: "2px solid #CCC"}} src={"/api/documents/" + selectedDocument} />
+		</div>;
 
+	}
+}
 
-import {Table, Label, Button, SplitButton, MenuItem, DropdownButton, ButtonGroup, Panel} from "react-bootstrap";
+Document.contextTypes = {
+	actions: React.PropTypes.object.isRequired,
+	router: React.PropTypes.object.isRequired
+}
 
 export default connect(state => {
 	return {
 		selectedDocument: state.selectedDocument
 	};
-})(React.createClass({
-			displayName: "Document",
-			contextTypes: {
-				actions: React.PropTypes.object.isRequired,
-				router: React.PropTypes.object.isRequired
-			},
-
-			render() {
-				var selectedDocument = this.props.selectedDocument;
-				return <div style={{height: "100%", width: "100%"}}>
-					<iframe style={{height: "100%", width: "100%", border: "2px solid #CCC"}} src={"/api/documents/" + selectedDocument} />
-				</div>;
-
-			}
-		}
-	)
-);
+})(Document);
 
 
 
